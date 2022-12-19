ff1d19e5437acd52595281ec647b3ad5140ded5a
eusouagabrielle <rodriguesgabrielle007@gmail.com>
2022-12-19 18:28:34 +0100

    Data file toevoeging

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,61 +1,61 @@
-/**INSERT INTO athletes (athleteId,
-                      athleteName,
+INSERT INTO trainers (age,
                       email,
-                      phoneNumber,
-                      age,
-                      startWeight,
-                      targetWeight,
-                      measurementRightArm,
-                      measurementLeftArm,
-                      measurementRightLeg,
-                      measurementLeftLeg,
-                      measurementWaist,
-                      trainer)
-VALUES (1,'Trevor','email@email.com','067453643','18','80.10','70.40','34','34','60','60','70',null),
-       (2,'Alice','email@email.nl','0685674323','18','80.10','70.40','34','34','60','60','70',null),
-       (3,'Amanda','email@email.br','0675934521','18','80.10','70.40','34','34','60','60','70',null);
-
-
-INSERT INTO trainers(personalTrainerId,
-                     trainersName,
-                     email,
-                     phoneNumber,
-                     kvkNumber,
-                     age,
-                     schoolDegree,
-                     yearsOfExperience,
-                     enrolledAthletes)
-VALUES (1,'Teresa','email@trainer.com','0673548234','23542916','18','HBO','3',null),
-       (2,'Max','email@trainer.nl','0673548234','23544916','18','HBO','3',null),
-       (3,'Teresa','email@trainer.br','0673548234','23242916','18','HBO','3',null);
-
-INSERT INTO exercises(exerciseId,
+                      kvk_number,
                       name,
-                      targetMuscle,
-                      sets,
-                      repetitions,
-                      weight,
-                      workoutRoutineList)
-VALUES (1,'exerciseone','back','3','4','23',null),
-       (2,'exercisetwo','chest','3','4','23',null),
-       (3,'exercisethree','back','3','4','23',null);
+                      phone_number,
+                      years_of_experience)
+VALUES ('18','marloes@email.com','26347542','marloes','0645234567','2'),
+       ('24','gabrielle@email.com','25632812','gabrielle','0645234875','1'),
+       ('20','margo@email.com','25432353','margo','0634532456','5');
 
-INSERT INTO routines(workoutRoutineId,
-                     name,
-                     description,
-                     durationInWeeks,
-                     difficultyLevel,
-                     exercises)
-VALUES ('1','routineone','a routine','3','hard',null),
-       ('2','routinetwo','a routine','3','hard',null),
-       ('3','routinethree','a routine','3','hard',null);
 
-**/
+INSERT INTO athletes (age,
+                      email,
+                      name,
+                      phone_number,
+                      start_weight,
+                      target_weight,
+                      trainer_id)
+VALUES ('16','trevor@email.com','trevor','0654362175','80.00','91.00','1'),
+       ('18','pim@email.com','pim','0654363476','55.70','80.00','1'),
+       ('30','samanta@email.com','samanta','0654095643','94.00','60.00','2'),
+       ('45','jan@email.com','teresa','0698753640','87.00','70.00','3');
 
-INSERT INTO users (username, password) VALUES ('gabrielle', '$2a$12$.BVZ/Y4B2IsiCXYQJGc7l.Wv0amfoH/2fgCAn3aIFYdrqBLZ0FEia');
-INSERT INTO users (username, password) VALUES ('bas', '$2a$12$1ZqrJ4uNzchupF8YB3xt2.H.qDOwNrYB6.eZjvTCQt/kRpEw8aqLS');
-INSERT INTO users (username, password) VALUES ('jan', '$2a$12$t8ebt9ML5Retezcn0Xs4OuD1hobJ8yVjgl7Yp9RXaQ1VVjUXvmoZW');
 
-INSERT INTO authorities (id, username, authority) VALUES (01, 'gabrielle', 'ROLE_ADMIN');
+INSERT INTO exercises (id,
+                       calorie,
+                       name,
+                       repetitions,
+                       sets,
+                       target_muscle,
+                       weight)
+VALUES (1,'10','exerciseone','10','3','back','23'),
+       (2,'15','exercisetwo','12','3','chest','15'),
+       (3,'30','exercisethree','12','3','biceps','10');
+
+INSERT INTO routines (id,
+                      description,
+                      difficulty_level,
+                      duration_in_weeks,
+                      name)
+VALUES ('1','a routine','easy','3','routineone'),
+       ('2','a routine','mild','3','routinetwo'),
+       ('3','a routine','hard','3','routinethree');
+
+INSERT INTO routine_exercises(routine_id,
+                              exercise_id)
+VALUES ('1','3'),
+       ('1','1'),
+       ('1','2'),
+       ('2','2'),
+       ('2','1'),
+       ('3','3'),
+       ('3','2');
+
+INSERT INTO users (username, password) VALUES ('gabrielle', '$2a$12$JCHbUV6Gy/d0fZiODfPqE.ERVvMtXmZT3Hx6Wzd0ti/daWzvjtTt6');
+INSERT INTO users (username, password) VALUES ('bas', '$2a$12$j3Lz996X8OV0MNBzq.a/POmvVm/A.VKsg./2WxFEthlBdVjKap39C');
+INSERT INTO users (username, password) VALUES ('jan', '$2a$12$iplmHdqUfpx2uEicVGQ.mect.1k2BCm9Xmv.hDgN3Zy5n.PabQu4u');
+
+INSERT INTO authorities (id, username, authority) VALUES (01, 'gabrielle', 'ROLE_DEFAULT');
 INSERT INTO authorities (id, username, authority) VALUES (02, 'bas', 'ROLE_ATHLETE');
 INSERT INTO authorities (id, username, authority) VALUES (03, 'jan', 'ROLE_TRAINER');
